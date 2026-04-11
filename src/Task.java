80200e26dff19181ccc0100527553f86466bfe43
Fedecastillom <fmascm20@gmail.com>
2026-04-11 01:46:35 -0300

    Creando atributos y constructor de task

--- src/Task.java
+++ src/Task.java
@@ -1,3 +1,14 @@
 public class Task {
+    private static int lastID = 0;
     private final int ID;
+    private String description;
+    private String status;
+    private String createdAt;
+    private String UpdatedAt;
+
+    public Task(String description) {
+        this.ID = ++lastID;
+        this.description = description;
+        this.status = "To do";
+    }
 }
