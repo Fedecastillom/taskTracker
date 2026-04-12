37ae84d0f962f69567cbc3c63705dbe11ea10677
Fedecastillom <fmascm20@gmail.com>
2026-04-12 00:17:33 -0300

    feat(metodos): markAs and getters

--- src/Task.java
+++ src/Task.java
@@ -1,14 +1,45 @@
+import java.time.LocalDateTime;
+
 public class Task {
     private static int lastID = 0;
     private final int ID;
     private String description;
     private String status;
-    private String createdAt;
-    private String UpdatedAt;
+    private final LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 
     public Task(String description) {
-        this.ID = ++lastID;
         this.description = description;
-        this.status = "To do";
+        ID = ++lastID;
+        status = "To do";
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    public void markAsDone() {
+        status = "Done";
+        updatedAt = LocalDateTime.now();
+    }
+
+    public void markInProgress() {
+        status = "In Progress";
+        updatedAt = LocalDateTime.now();
+    }
+
+    public void updateDescription(String description) {
+        this.description = description;
+        updatedAt = LocalDateTime.now();
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public String toString() {
+        return "id: " + ID + ", description: " + description + ", status: " + status + ", created At: " + createdAt + ", updated At: " + updatedAt;
     }
 }
